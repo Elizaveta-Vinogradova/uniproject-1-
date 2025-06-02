947bfbe1f842aa5c22b4f8b61ead098aebd3af2f
Elizaveta Vinogradova <lizaivinograd@gmail.com>
2025-06-02 15:07:44 +0500

    final fix

---
+++ Assets/Scripts/Traps/NewSecretArea.cs
@@ -0,0 +1,49 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class NewSecretArea : MonoBehaviour
+{
+    [Header("Disappearance settings")]
+    [SerializeField] private float fadeDuration = 2f;   
+    [SerializeField] private float visibleAlpha = 1f;
+    
+    private SpriteRenderer spriteRenderer;
+    private Coroutine currentFadeCoroutine;
+
+    private void Start()
+    {
+        spriteRenderer = GetComponent<SpriteRenderer>();
+        
+        var initialColor = spriteRenderer.color;
+        spriteRenderer.color = new Color(initialColor.r, initialColor.g, initialColor.b, 0f);
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag("Player"))
+            currentFadeCoroutine = StartCoroutine(FadeSprite(visibleAlpha));
+    }
+    
+    private void OnTriggerExit2D(Collider2D other)
+    {
+        if (other.CompareTag("Player"))
+            currentFadeCoroutine = StartCoroutine(FadeSprite(0f));
+    }
+
+    private IEnumerator FadeSprite(float targetAlpha)
+    {
+        var startColor = spriteRenderer.color;
+        var targetColor = new Color(startColor.r, startColor.g, startColor.b, targetAlpha);
+        
+        var elapsedTime = 0f;
+        
+        while (elapsedTime < fadeDuration)
+        {
+            var newAlpha = Mathf.Lerp(startColor.a, targetAlpha, elapsedTime / fadeDuration);
+            spriteRenderer.color = new Color(startColor.r, startColor.g, startColor.b, newAlpha);
+            elapsedTime += Time.deltaTime;
+            yield return null;
+        }
+        spriteRenderer.color = targetColor;
+    }
+}
---
+++ Assets/Scripts/Traps/Torch.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using UnityEngine;
+
+public class LanternPuzzle : MonoBehaviour
+{
+    public int orderID; 
+    public GameObject hiddenPlatform; 
+    public float transitionSpeed = 2.0f;
+    private Player player;
+    private SpriteRenderer spriteRenderer;
+    private Color targetColor;
+    private static LanternPuzzle[] allLanterns;
+    private static int currentOrder = 1;
+
+    private void Start()
+    {
+        var playerObject = GameObject.Find("Player");
+        if (playerObject != null)
+            player = playerObject.GetComponent<Player>();
+        spriteRenderer = GetComponent<SpriteRenderer>();
+        allLanterns = FindObjectsOfType<LanternPuzzle>();
+        targetColor = Color.gray; 
+        spriteRenderer.color = targetColor;
+    }
+    
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Q))
+        {
+            var distance = Vector3.Distance(transform.position, player.transform.position);
+            if (distance < 3f)
+                ToggleLantern();
+        }
+
+        if (spriteRenderer.color != targetColor)
+            spriteRenderer.color = Color.Lerp(spriteRenderer.color, targetColor, Time.deltaTime * transitionSpeed);
+    }
+
+    private void ToggleLantern()
+    {
+        if (spriteRenderer.color != Color.gray && spriteRenderer.color != Color.white) return;
+        var isCurrentlyLit = (spriteRenderer.color == Color.white);
+        if (!isCurrentlyLit)
+        {
+            if (orderID == currentOrder)
+            {
+                targetColor = Color.white;
+                currentOrder++;
+                CheckPuzzleComplete();
+            }
+            else
+                ResetAllLanterns();
+        }
+        else
+            targetColor = Color.gray;
+    }
+
+    private void CheckPuzzleComplete()
+    {
+        if (currentOrder > allLanterns.Length)
+            hiddenPlatform.SetActive(true);
+    }
+
+    private void ResetAllLanterns()
+    {
+        currentOrder = 1;
+        foreach (var lantern in allLanterns)
+        {
+            lantern.targetColor = Color.gray;
+        }
+    }
+}
